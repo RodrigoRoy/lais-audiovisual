a34ae715e69071e03d29cd73b667e9a8a754ce7b
sergioarc <sergioar@ciencias.unam.mx>
2015-06-04 11:09:44 -0500

    Prueba n

--- public/php/manejoBD.php
+++ public/php/manejoBD.php
@@ -4,7 +4,6 @@ include 'conexion.php';
 	
 
 
-
 /*Funcion que muestra los datos completos de cada archivo audiovisual*/
 function mostrar(){
 	$select = "SELECT * FROM area_de_identificacion";
@@ -25,145 +24,9 @@ function mostrar(){
     }
 }
 
-function agregar(){
-    $_POST['duracion'] = setDuracion($_POST['duracion']);
-    
-    if(isset($_POST['fuentes'])){
-        $_POST['fuentes'] = setFuenteRecurso($_POST['fuentes']);
-    }else{
-        $_POST['fuentes'] = '';
-    }
-    
-    if(isset($_POST['recursos'])){
-        $_POST['recursos'] = setFuenteRecurso($_POST['recursos']);
-    }else{
-        $_POST['recursos'] = '';
-    }
-
-    $identificacion = "INSERT INTO area_de_identificacion() VALUES('"
-            . $_POST['codigo_de_referencia'] . "','"
-            . $_POST['titulo_propio'] . "','" 
-            . $_POST['titulo_paralelo'] . "','" 
-            . $_POST['titulo_atribuido'] . "','" 
-            . $_POST['titulo_de_serie'] . "','"
-            . $_POST['numero_de_programa'] . "','"
-            . $_POST['pais'] . "','"
-            . $_POST['fecha'] . "',"
-            . $_POST['duracion'] . ",'"
-            . $_POST['investigacion'] . "','"
-            . $_POST['realizacion'] . "','"
-            . $_POST['direccion'] . "','"
-            . $_POST['guion'] . "','"
-            . $_POST['adaptacion'] . "','"
-            . $_POST['idea_original'] . "','"
-            . $_POST['fotografia'] . "','"
-            . $_POST['fotografia_fija'] . "','"
-            . $_POST['edicion'] . "','"
-            . $_POST['sonido_grabacion'] . "','"
-            . $_POST['sonido_edicion'] . "','"
-            . $_POST['musica_original'] . "','"
-            . $_POST['musicalizacion'] . "','"
-            . $_POST['voces'] . "','"
-            . $_POST['actores'] . "','"
-            . $_POST['animacion'] . "','"
-            . $_POST['otros_colaboradores']
-            . "');";
-
-        $contexto = "INSERT INTO area_de_contexto() VALUES('"
-            . $_POST['codigo_de_referencia'] . "','"
-            . $_POST['entidad_productora'] . "','"
-            . $_POST['productor'] . "','"
-            . $_POST['distribuidora'] . "','"
-            . $_POST['historia_institucional'] . "','"
-            . $_POST['resena_biografica'] . "','"
-            . $_POST['forma_de_ingreso'] . "','"
-            . $_POST['fecha_de_ingreso'] 
-            . "');";
-        
-        // Los campos de Fuentes y Recursos son un arreglo con múltiples valores y se utiliza la función
-        // implode() para convertir en cadena de texto y delimitando por comas (,) cada valor.
-        $contenido = "INSERT INTO area_de_contenido_y_estructura() VALUES('"
-            . $_POST['codigo_de_referencia'] . "','"
-            . $_POST['sinopsis'] . "','"
-            . $_POST['descriptor_onomastico'] . "','"
-            . $_POST['descriptor_toponimico'] . "','"
-            . $_POST['descriptor_cronologico'] . "','"
-            . $_POST['tipo_de_produccion'] . "','"
-            . $_POST['genero'] . "','"
-            . $_POST['fuentes'] . "','"
-            . $_POST['recursos'] . "','"
-            . $_POST['versiones'] . "','"
-            . $_POST['formato_original'] . "','"
-            . $_POST['material_extra'] 
-            . "');";
-        
-        $condiciones = "INSERT INTO area_de_condiciones_de_acceso() VALUES('"
-            . $_POST['codigo_de_referencia'] . "','"
-            . $_POST['condiciones_de_acceso'] . "','"
-            . $_POST['existencia_y_localizacion_de_originales'] . "','"
-            . $_POST['idioma_original'] . "','"
-            . $_POST['doblajes_disponibles'] . "','"
-            . $_POST['subtitulajes'] . "','"
-            . $_POST['soporte'] . "','"
-            . $_POST['numero_copias'] . "','"
-            . $_POST['descripcion_fisica'] . "','"
-            . $_POST['color'] . "','"
-            . $_POST['audio'] . "','"
-            . $_POST['sistema_de_grabacion'] . "','"
-            . $_POST['region_dvd'] . "','"
-            . $_POST['requisitos_tecnicos'] 
-            . "');";
-        
-        $documentacion = "INSERT INTO area_de_documentacion_asociada() VALUES('"
-            . $_POST['codigo_de_referencia'] . "','"
-            . $_POST['existencia_y_localizacion_de_copias'] . "','"
-            . $_POST['unidades_de_descripcion_relacionadas'] . "','"
-            . $_POST['documentos_asociados'] 
-            . "');";
-        
-        $notas = "INSERT INTO area_de_notas() VALUES('"
-            . $_POST['codigo_de_referencia'] . "','"
-            . $_POST['area_de_notas'] 
-            . "');";
-
-        $descripcion = "INSERT INTO area_de_descripcion() VALUES('"
-            . $_POST['codigo_de_referencia'] . "','"
-            . $_POST['notas_del_archivero'] . "','"
-            . $_POST['datos_del_archivero'] . "','"
-            . $_POST['reglas_o_normas'] . "','"
-            . $_POST['fecha_de_descripcion'] 
-            . "');";
-
-        // Mostrar en página las consultas realizadas (para revisar sintáxis)
-        /*echo '<pre>' . $identificacion . '</pre>';
-        echo '<pre>' . $contexto . '</pre>';
-        echo '<pre>' . $contenido . '</pre>';
-        echo '<pre>' . $condiciones . '</pre>';
-        echo '<pre>' . $documentacion . '</pre>';
-        echo '<pre>' . $notas . '</pre>';
-        echo '<pre>' . $descripcion . '</pre>';*/
-        
-        try{
-            $conn->exec($identificacion);
-            $conn->exec($contexto);
-            $conn->exec($contenido);
-            $conn->exec($condiciones);
-            $conn->exec($documentacion);
-            $conn->exec($notas);
-            $conn->exec($descripcion);
-
-            echo '<div class="alert alert-success" role="alert"><p>New record created successfully</p><p>View the record <a href="vista.php?id=' 
-            . $_POST['codigo_de_referencia'] . '">here</a></p></div>';
-        }
-        catch(PDOException $e){
-            echo $e->getMessage();
-        }
-
-        $conn = null;
-}
 
 /*Funcion que agrega un nuevo archivo audivisual*/
-function agregar2(){
+function agregar(){
     $datos = json_decode(file_get_contents("php://input"));
     //print_r($datos);
     //Propiedades del área de identificación
