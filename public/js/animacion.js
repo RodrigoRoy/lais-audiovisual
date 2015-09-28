cafdeda127017b7aa07b207673c125794825bebf
sergioarc <sergioar@ciencias.unam.mx>
2015-09-28 16:33:43 -0500

    :D

---
+++ public/js/animacion.js
@@ -0,0 +1,14 @@
+// Cuando el documento esté listo se realizan todas las llamadas a las funciones que involucran al formulario.
+	//Iniciar el navbar
+$( ".navegador" ).css( "background-color", "transparent");
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 110) {
+                    $(".navegador").css("background-color", "rgba(0,0,0,.8)");
+                }else{
+                    $( ".navegador" ).css( "background-color", "transparent");
+                }
+            });
+});
